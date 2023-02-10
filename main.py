70692641d288478745099eaea9c5720269424c9e
Pan4ikk <dyatel.maksimka@gmail.com>
2023-02-10 17:30:38 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 class Student:
-    def __init__(self, height=160):
-        self.height = height
-nick = Student()
-kate = Student(height=170)
-sheldon = Student(height=190)
-print(nick.height)
-print(kate.height)
-print(sheldon.height)
+    print("Здравствуйте, Роман!")
