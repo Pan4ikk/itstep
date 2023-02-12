de694f19e5d3f8575551143d2544e38a624e54d8
Pan4ikk <dyatel.maksimka@gmail.com>
2023-02-12 17:02:13 +0200

    initial commit

--- clicker.py
+++ clicker.py
@@ -1,9 +1,34 @@
 import tkinter as tk
 
+level = 1
+coins = 0
+hp = 50
+
 root = tk.Tk()
-root.title("CLicker Wars")
+root.title("Cat Wars")
 root.geometry("600x800")
 #root.iconbitmap(like.ico)
 root.resizable(False, False)
+
+lvl_1 = tk.PhotoImage(file="1.png")
+lvl_2 = tk.PhotoImage(file="1.png")
+lvl_3 = tk.PhotoImage(file="1.png")
+lvl_4 = tk.PhotoImage(file="1.png")
+lvl_5 = tk.PhotoImage(file="1.png")
+
+def click():
+    print("hit")
+
+title = tk.Label(font = ("Arial", 25, "bold"), text="Cat Wars", fg = "orange")
+title.pack()
+lvl = tk.Label(font = ("Arial", 14), text=f"Lvl {level}")
+lvl.pack()
+coins = tk.Label(font = ("Arial", 14), text=f"Coins: {coins}", fg="gold", bg="gray")
+coins.pack()
+
+click_button = tk.Button(root, image=lvl_1, command=click)
+click_button.pack()
+hp = tk.Label(font = ("Arial", 14), text=f"HP: {hp}", fg="red")
+hp.pack()
 
 root.mainloop()
